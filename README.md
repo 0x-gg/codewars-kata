89d3757bb970e637e52598f83e427c8bab63e37a
Gaurav Gada <Gaurav Gada>
2016-10-27 01:32:10 -0700

    Init

---
+++ README.md
@@ -0,0 +1,11 @@
+## The Vigenère cipher
+
+A cipher that survived 3 centuries of decipher attempts & earned the name _le chiffre indéchiffrable_ implemented in JavaScript!
+
+##### [kata link](https://www.codewars.com/kata/52d2e2be94d26fc622000735)
+
+- 10 October 2016
+- 3 kyu
+- [Airbnb style guide](https://github.com/airbnb/javascript) 
+- Note: ```[... % abc.length()];```
+- Use of ```let, const```
---
+++ VigenereAutokeyCipher.js
@@ -0,0 +1,49 @@
+/*jshint esversion: 6 */
+
+function VigenèreAutokeyCipher(key, abc) {
+    console.log("key: ", key, "abc: ", abc);
+    this.encode = function(str) {
+        console.log("enc: ", str);
+        let i = 0;
+        let newKey = key + str.split('').map(x => {
+            return inABC(x) ? x : '';
+        }).join('');
+        return str.trim().split('').map((x, idx) => {
+            if (inABC(x)) {
+                keyChar = newKey[i];
+                i++;
+            } else return x;
+            return abc[(abc.indexOf(keyChar) + abc.indexOf(x)) % abc.length];
+        }).join('');
+    };
+    this.decode = function(str) {
+        console.log("dec: ", str);
+        let i = 0;
+        let newKey = key;
+        return str.trim().split('').map((x, idx) => {
+            if (inABC(x)) {
+                let decodedChar = decodeChar(abcIndex(x), abcIndex(newKey[i]));
+                i++;
+                newKey = newKey + decodedChar;
+                return decodedChar;
+            } else return x;
+        }).join('');
+    };
+
+    const inABC = function checkinABC(chr) {
+        return abc.indexOf(chr) >= 0 ? true : false;
+    };
+
+    const abcIndex = function getabcIndex(chr) {
+        return abc.indexOf(chr);
+    };
+
+    const keyPos = function getkeyPos(idx) {
+        return idx % key.length;
+    };
+
+    const decodeChar = function dodecodeChar(abcIdx, keyIdx) {
+        if (abcIdx >= keyIdx) return abc[abcIdx - keyIdx];
+        else return abc[abc.length + abcIdx - keyIdx];
+    };
+}
